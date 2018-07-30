83fe7d459d93527ae00617fa361913d042f1e09b
Sarah <yamashige.sarah@gmail.com>
2018-07-29 23:04:05 -1000

    Linked app.js file to html and started with a sanity check

---
+++ js/app.js
@@ -0,0 +1,16 @@
+console.log("Testing My Sanity")
+
+// Get a Person object from the SWAPI: https://swapi.co/api/people/4
+// Fill in "person4Name" with the value of Person's name
+//Fill in "person4Homeworld" with the value of Person's "homeworld" (name)
+
+
+// Get a Person object from the SWAPI: https://swapi.co/api/people/14
+// Fill in "person14Name" with the value of Person's "name"
+// Fill in "person14Species" with teh value of Person's (first) "species" (name)
+
+// Geta  list of all the films from the SWAPI: https://swapi.co/api/films
+// Fill in "filmList" with a new "li" element for each "film"
+// Fill in each film's "filmTitle" with the title of the "film"
+// Create a new "li" in this film's "filmPlanets" for each "planet" that appaared in this "film"
+// Fill in each "planetTitle" with the name of the "planet"
